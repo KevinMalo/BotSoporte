dd7e366a3155e7a690844c30adeef57f9a406d46
Kevin Bueno <kean.solano@gmail.com>
2017-11-09 10:21:28 -0500

    Confirmacion de formulario enviado

--- app.js
+++ app.js
@@ -2,20 +2,26 @@ var express = require('express');
 var app = express();
 var path    = require("path");
 
+//npm install unirest para hacer las peticiones GET y POST
 var unirest = require('unirest');
 
+//npm install body-parser para obtener los datos desde el html
 var bodyParser = require('body-parser');
 
-//app.use('/css', express.static(__dirname + '/css'));
-//Store all JS and CSS in Scripts folder.
+//Carga de archivos estaticos de la carpeta Public
 
 app.use(express.static('public'));
 
-
-
+//Ruta y render de nuestro archivo HTML
 app.get('/', function (req, res) {
     
     res.sendFile(path.join(__dirname+'/index.html'));
+
+});
+
+app.get('/formulario', function (req, res) {
+    
+    res.sendFile(path.join(__dirname+'/formulario.html'));
 
 });
 
@@ -53,7 +59,7 @@ app.post('/formulario', function (req, res) {
 
     var payload = {"contexts":[""],"events":[],"fallbackIntent":false,"name":categoria,"priority":500000,"responses":[{"action":"add.list","affectedContexts":[],"defaultResponsePlatforms":{"google":true},"messages":[{"platform":"google","textToSpeech":"Okay","type":"simple_response"},{"speech":respuestas,"type":0}],"parameters":[{"dataType":"0","isList":true,"name":"0","prompts":["li"],"required":true,"value":"0"}],"resetContexts":false}],"templates":["c:c ","b:b ","a:a "],"userSays":preguntas,"webhookForSlotFilling":false,"webhookUsed":false}
     
-    
+    //Unirest: Peticion POST a la API de DialogFlow
     unirest.post('https://api.dialogflow.com/v1/intents?v=20150910')
     .headers({
         'Authorization':'Bearer af8f01865f994a5186d4ae0b0b250d11', 
@@ -67,14 +73,6 @@ app.post('/formulario', function (req, res) {
 })
 
 //////
-
-
-
-
-
-
-
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
